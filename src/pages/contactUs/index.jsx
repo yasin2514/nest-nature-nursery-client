a7a53f96d6499f5b1a79ebd55e45f52f3714609b
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-20 12:23:40 +0600

    banner added

--- src/pages/contactUs/index.jsx
+++ src/pages/contactUs/index.jsx
@@ -1,8 +1,13 @@
+import ShortBanner from "../../components/ui/ShortBanner";
 
 const ContactUS = () => {
   return (
     <div>
-      <h1>ContactUS page</h1>
+      <ShortBanner
+        banner={"all-plants-banner-2"}
+        header={"Contact Us"}
+        text={`Welcome to Next Nature Nursery, your go-to for vibrant, healthy plants. Find flowers, herbs, succulents, and more to enhance your indoor and outdoor spaces.`}
+      />
     </div>
   );
 };
--- src/pages/plantCategory/index.jsx
+++ src/pages/plantCategory/index.jsx
@@ -4,7 +4,7 @@ const PlantCategory = () => {
   return (
     <div>
       <ShortBanner
-        banner={"all-plants-banner"}
+        banner={"all-plants-banner-3"}
         header={"All Plant Category"}
         text={`Welcome to Next Nature Nursery, your go-to for vibrant, healthy plants. Find flowers, herbs, succulents, and more to enhance your indoor and outdoor spaces.`}
       />
