1e595f22ea3b81d7664fa9d68815f567b8ac5f23
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-21 18:46:36 +0600

    dashboard coppase done

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 import LoginAndLogout from "../ui/LoginAndLogout";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-const Header = () => {
+const Header = ({ collapse, toggleCollapse }) => {
   return (
     <div className="h-[70px] bg-[#01352c] text-white flex justify-between items-center px-7">
-      <div className="flex gap-5">
-        <h1>Header</h1>
+      <div className="flex items-center gap-5">
+        <span className="cursor-pointer" onClick={toggleCollapse}>
+          {!collapse ? (
+            <FaArrowRight className="hover:text-[#33ff5f] text-xl" />
+          ) : (
+            <FaArrowLeft className="hover:text-[#33ff5f] text-xl" />
+          )}
+        </span>
         <Link to={"/"} className="hover:text-[#33ff5f]">
           Home
         </Link>
--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -2,16 +2,35 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/dashboard/Sidebar";
 import Header from "../components/dashboard/Header";
 import SidebarHeader from "../components/dashboard/SidebarHeader";
+import { useState } from "react";
 
 const AdminLayout = () => {
+  const [collapse, setCollapse] = useState(false);
+
+  const toggleCollapse = () => {
+    setCollapse((prev) => !prev);
+  };
+
   return (
-    <div className="grid grid-cols-12 h-screen">
-      <div className="col-span-2 bg-[#01352c]">
-        <SidebarHeader />
-        <Sidebar />
+    <div className="h-screen flex transition-all duration-500">
+      <div
+        className={`bg-[#01352c] transition-all duration-500 ${
+          collapse ? "w-1/6" : "w-0"
+        } overflow-hidden`}
+      >
+        {collapse && (
+          <div className="p-4">
+            <SidebarHeader />
+            <Sidebar />
+          </div>
+        )}
       </div>
-      <div className="col-span-10">
-        <Header />
+      <div
+        className={`flex-1 transition-all duration-500 ${
+          collapse ? "w-5/6" : "w-full"
+        }`}
+      >
+        <Header collapse={collapse} toggleCollapse={toggleCollapse} />
         <div className="p-7">
           <Outlet />
         </div>
