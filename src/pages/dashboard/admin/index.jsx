acf7ee87a66c9a31755337253f522f67cd932448
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-21 23:59:58 +0600

    admin dashboard home design done

--- src/pages/dashboard/admin/index.jsx
+++ src/pages/dashboard/admin/index.jsx
@@ -6,10 +6,14 @@ const AdminDashboard = () => {
     <div>
       <BreadCum text1={"Admin Dashboard"} text2={"Home"} />
       <div className="mt-10 grid grid-cols-12 gap-8">
-        <Box value={10} title={"Total Users"} className={"bg-[#22661d]"} />
-        <Box value={10} title={"Total Users"} className={"bg-[#007bac]"} />
-        <Box value={10} title={"Total Users"} className={"bg-[#824a00]"} />
-        <Box value={10} title={"Total Users"} className={"bg-[#00a2ff]"} />
+        <Box value={10} title={"Total Users"} className={"bg-[#2c8d2c]"} />
+        <Box value={10} title={"Total Users"} className={"bg-[#16a7c0]"} />
+        <Box value={10} title={"Total Users"} className={"bg-[#786fc4]"} />
+        <Box value={10} title={"Total Users"} className={"bg-[#ce4b3a]"} />
+      </div>
+      <div className="grid grid-cols-12 mt-10 gap-8">
+        <div className="bg-white col-span-8 p-5 rounded-lg shadow-md h-[calc(100vh-400px)]">text</div>
+        <div className="bg-white col-span-4 p-5 rounded-lg shadow-md h-[calc(100vh-400px)]">text</div>
       </div>
     </div>
   );
