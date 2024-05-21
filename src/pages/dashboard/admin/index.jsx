53e13930e7382345dc408563008215f7efff4d41
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-22 01:05:31 +0600

    admin home dashboard done

--- src/pages/dashboard/admin/index.jsx
+++ src/pages/dashboard/admin/index.jsx
@@ -1,3 +1,4 @@
+import BarChartComponent from "../../../components/dashboard/BarChartComponent";
 import Box from "../../../components/dashboard/Box";
 import BreadCum from "../../../components/dashboard/BreadCum";
 import PieChart from "../../../components/dashboard/PieChart";
@@ -6,20 +7,26 @@ const AdminDashboard = () => {
   return (
     <div className=" h-full">
       <BreadCum text1={"Admin Dashboard"} text2={"Home"} />
-      <div className="flex flex-col justify-around h-[calc(100vh-180px)]">
-        <div className="mt-10 grid grid-cols-12 gap-8">
+      <div className="flex flex-col justify-around h-[calc(100vh-165px)]">
+        <div className="mt-8 grid grid-cols-12 gap-8">
           <Box value={10} title={"Total Users"} className={"bg-[#2c8d2c]"} />
           <Box value={10} title={"Total Users"} className={"bg-[#16a7c0]"} />
           <Box value={10} title={"Total Users"} className={"bg-[#786fc4]"} />
           <Box value={10} title={"Total Users"} className={"bg-[#ce4b3a]"} />
         </div>
         <div className="grid grid-cols-12  gap-8 mt-10">
-          <div className="bg-white col-span-8 p-5 rounded-lg shadow-md "></div>
+          {/* for sales chart */}
+          <div className="bg-white col-span-8 p-5 rounded-lg shadow-md flex flex-col items-center justify-center">
+            <BarChartComponent />
+            <h2 className="text-2xl text-green-800 font-semibold">
+              Weekly Sales Chart
+            </h2>
+          </div>
 
           {/* for pie chart */}
-          <div className="bg-white col-span-4 px-5 py-8 rounded-lg shadow-md h-auto  flex flex-col justify-center items-center gap-5">
+          <div className="bg-white col-span-4  rounded-lg shadow-md h-auto  flex flex-col justify-center items-center gap-5">
             <PieChart />
-            <div className="text-center">
+            <div className="text-center space-y-5">
               <p className="text-center text max-w-[20ch] mx-auto text-gray-700 font-semibold">
                 Summary based on categories of plants sold in the last 30 days
               </p>
