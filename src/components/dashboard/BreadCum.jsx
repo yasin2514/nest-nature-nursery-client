fb6bf4f8b99ae1d69b2191077c75a23377e24954
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-22 01:15:06 +0600

    updated

--- src/components/dashboard/BreadCum.jsx
+++ src/components/dashboard/BreadCum.jsx
@@ -1,6 +1,6 @@
 const BreadCum = ({ text1, text2 }) => {
   return (
-    <div className="bg-white px-5 py-3 rounded-lg shadow-sm flex justify-between items-center text-lg font-semibold text-green-500">
+    <div className="bg-white px-5 py-2 rounded-lg shadow-sm flex justify-between items-center text-lg font-semibold text-green-500">
       <span>{text2}</span>
       <div className="">
         <span>Dashboard</span> <span>/</span> <span>{text1}</span>
