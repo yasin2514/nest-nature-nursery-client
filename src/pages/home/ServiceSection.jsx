dc5756a7e7adf166826452d40e3b99c07357085c
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-24 22:23:03 +0600

    customer services section done

--- src/pages/home/ServiceSection.jsx
+++ src/pages/home/ServiceSection.jsx
@@ -1,5 +1,9 @@
 import { FaShippingFast } from "react-icons/fa";
 import SectionHeader from "../../components/ui/SectionHeader";
+import { FaSackDollar } from "react-icons/fa6";
+import { GiDeliveryDrone } from "react-icons/gi";
+import { IoIosCall } from "react-icons/io";
+import { MdAssignmentReturn } from "react-icons/md";
 
 const ServiceSection = () => {
   return (
@@ -11,33 +15,33 @@ const ServiceSection = () => {
       <div className="grid grid-cols-5 gap-10">
         <Section
           title="Free Shipping"
-          text="We provide free shipping to all over the world"
+          text="We provide free shipping some plants."
         >
           <FaShippingFast className="text-4xl text-green-600" />
         </Section>
         <Section
-          title="Free Shipping"
-          text="We provide free shipping to all over the world"
+          title="Delivery On"
+          text="We provide 24/7 hours delivery on  any place."
         >
-          <FaShippingFast className="text-4xl text-green-600" />
+          <GiDeliveryDrone className="text-4xl text-green-600" />
         </Section>
         <Section
-          title="Free Shipping"
-          text="We provide free shipping to all over the world"
+          title="Payment"
+          text="We provide online and cash on payment system."
         >
-          <FaShippingFast className="text-4xl text-green-600" />
+          <FaSackDollar className="text-4xl text-green-600" />
         </Section>
         <Section
-          title="Free Shipping"
-          text="We provide free shipping to all over the world"
+          title="Need Help"
+          text="If you need any help feel free to call or email us."
         >
-          <FaShippingFast className="text-4xl text-green-600" />
+          <IoIosCall className="text-4xl text-green-600" />
         </Section>
         <Section
-          title="Free Shipping"
-          text="We provide free shipping to all over the world"
+          title="Return"
+          text="We provide free return and exchange our plants."
         >
-          <FaShippingFast className="text-4xl text-green-600" />
+          <MdAssignmentReturn className="text-4xl text-green-600" />
         </Section>
       </div>
     </div>
@@ -46,13 +50,13 @@ const ServiceSection = () => {
 
 const Section = ({ title, text, children }) => {
   return (
-    <div className="flex flex-col items-center justify-center rounded-md hover:scale-110 overflow-hidden duration-500 hover:outline p-5 text-center space-y-2">
+    <div className="flex flex-col items-center justify-center rounded-md hover:scale-110 overflow-hidden duration-500 hover:outline hover:outline-[#01352c] p-5 text-center space-y-2">
       <div className="bg-gray-100 p-2 rounded-full">
         <div className="bg-gray-200 p-7 rounded-full">{children}</div>
       </div>
       <div>
         <h2 className="text-2xl font-semibold">{title}</h2>
-        <p className="text-gray-700 max-w-[20ch]">{text}</p>
+        <p className="text-gray-700 max-w-[40ch] mt-1 ">{text}</p>
       </div>
     </div>
   );
