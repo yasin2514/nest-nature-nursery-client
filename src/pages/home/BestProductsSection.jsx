5c9fe3c63daa164a64246a83625122c2f6920f34
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-24 12:11:33 +0600

    our best plant section done

--- src/pages/home/BestProductsSection.jsx
+++ src/pages/home/BestProductsSection.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import ProductCard from "../../components/ui/ProductCard";
+import SectionHeader from "../../components/ui/SectionHeader";
+import useGetProducts from "../../hooks/useGetProducts";
 
 const BestProductsSection = () => {
-    return (
-      <div>
-        <h1>BestProductsSection</h1>
+  const [products] = useGetProducts();
+
+  return (
+    <div>
+      <div className="flex justify-between items-start">
+        <SectionHeader header={"Our Best Plants"} />
+        <div>
+          <Link to="/all-plants">Vew All</Link>
+        </div>
       </div>
-    );
+      <div className={"grid grid-cols-4 gap-8"}>
+        {products?.slice(0, 8).map((product, index) => {
+          return (
+            <div key={index} className={""}>
+              <ProductCard data={product} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default BestProductsSection;
