ff90a981411630420c049f104a0364978647213e
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-25 11:32:57 +0600

    weekly best deal updated

--- src/components/ui/ProductsCardTwo.jsx
+++ src/components/ui/ProductsCardTwo.jsx
@@ -1,7 +1,6 @@
 import { FiShoppingCart } from "react-icons/fi";
 import useNumberFormatter from "../../hooks/useNumberFormatter";
 import useRating from "../../hooks/useRating";
-import { FaShoppingCart } from "react-icons/fa";
 
 const ProductsCardTwo = ({ data }) => {
   const { _id, name, price, photo, rating, previousPrice } = data;
@@ -10,8 +9,8 @@ const ProductsCardTwo = ({ data }) => {
   const renderRating = useRating();
 
   return (
-    <div className="border rounded-lg hover:shadow-lg shadow-green-700  ">
-      <div className="h-48 bg-gray-50  overflow-hidden ">
+    <div className="border rounded-xl overflow-hidden hover:shadow-lg shadow-green-700  ">
+      <div className="h-48 bg-gray-100 overflow-hidden ">
         <img
           src={photo[0]}
           alt={name}
@@ -21,13 +20,17 @@ const ProductsCardTwo = ({ data }) => {
       <div className="p-5">
         <h4 className="text-lg font-bold mb-2">{name}</h4>
         <div className="flex items-center">
-          <span className="text-yellow-500">★★★★★</span>
-          <span className="ml-2 text-gray-600">({rating})</span>
+          {renderRating(rating)}
+          <span className="ml-2 text-gray-600">{rating}</span>
         </div>
         <div className="flex items-center justify-between mb-2">
           <div className="space-x-3">
-            <span className="text-gray-600 font-bold">${price}</span>
-            <span className="text-gray-400 line-through">${previousPrice}</span>
+            <span className="text-gray-600 font-bold">
+              ${formatNumber(price)}
+            </span>
+            <span className="text-gray-400 line-through">
+              ${formatNumber(previousPrice)}
+            </span>
           </div>
           <div className="flex items-center justify-between">
             <button className="btn btn-sm">
