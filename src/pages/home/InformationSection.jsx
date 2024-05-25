ce078b5594689ce55d3b4d6cfbb90f40e0c16e87
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-25 13:38:48 +0600

    inforamation section done

---
+++ src/pages/home/InformationSection.jsx
@@ -0,0 +1,37 @@
+import CountUp from "react-countup";
+import bg from "../../assets/banner-3.jpg";
+import Container from "../../components/ui/Container";
+const InformationSection = () => {
+  return (
+    <div
+      className=" bg-fixed h-[70vh] flex items-center justify-center bg-no-repeat bg-cover "
+      style={{
+        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.40), rgba(0, 0, 0, 0.60)),url(${bg})`,
+      }}
+    >
+      <Container>
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-4 items-center w-full rounded-lg py-10 text-white bg-black bg-opacity-80">
+          <Section value={"1050"} title={"Total Plants"} />
+          <Section value={"510"} title={" Delivery Plants"} />
+          <Section value={"150"} title={"Total Clients"} />
+          <Section value={"160"} title={"Plant Category"} />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+const Section = ({value,title}) => {
+  return (
+    <div className="text-center space-y-5 ">
+      <CountUp
+        className="text-5xl font-bold text-green-500"
+        end={value}
+        duration={2.75}
+      />
+      <h4 className="text-2xl font-bold uppercase">{title}</h4>
+    </div>
+  );
+};
+
+export default InformationSection;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,24 +3,28 @@ import BestProductsSection from "./BestProductsSection";
 import ClientReviewSection from "./ClientReviewSection";
 import HeroSection from "./HeroSection";
 import HowToOrderSection from "./HowToOrderSection";
+import InformationSection from "./InformationSection";
 import OurNurserySection from "./OurNurserySection";
 import ServiceSection from "./ServiceSection";
 import TrendingProductsSection from "./TrendingProductsSection";
 
 const Home = () => {
-    return (
-      <>
-        <HeroSection />
-        <Container className="my-24 space-y-24">
-          <OurNurserySection/>
-          <TrendingProductsSection/>
-          <HowToOrderSection/>
-          <ServiceSection/>
-          <BestProductsSection/>
-          <ClientReviewSection/>
-        </Container>
-      </>
-    );
+  return (
+    <>
+      <HeroSection />
+      <Container className="my-24 space-y-24">
+        <OurNurserySection />
+        <TrendingProductsSection />
+        <HowToOrderSection />
+        <ServiceSection />
+      </Container>
+      <InformationSection />
+      <Container className="my-24 space-y-24">
+        <BestProductsSection />
+        <ClientReviewSection />
+      </Container>
+    </>
+  );
 };
 
 export default Home;
