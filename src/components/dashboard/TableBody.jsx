4308fa29935e8f01fece9ca867beaaf4bed4328b
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-29 23:44:57 +0600

    product deleted successfully

--- src/components/dashboard/TableBody.jsx
+++ src/components/dashboard/TableBody.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import useNumberFormatter from "../../hooks/useNumberFormatter";
 import { AiFillDelete } from "react-icons/ai";
 import { FaEdit } from "react-icons/fa";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import useGetProducts from "../../hooks/useGetProducts";
+import useGetDataUploadedByAdmin from "../../hooks/useGetDataUploadedByAdmin";
 
 const TableBody = ({ data, index, show }) => {
   const {
@@ -15,8 +19,35 @@ const TableBody = ({ data, index, show }) => {
     email,
     role,
     uploadByEmail,
+    edited,
   } = data || {};
   const formatNumber = useNumberFormatter();
+  const axiosSecure = useAxiosSecure();
+  const [, , refetchAll] = useGetProducts();
+  const [, , refetchAdmin] = useGetDataUploadedByAdmin();
+
+  // delete user
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: `Are you sure to delete this Plant?`,
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`deleteProduct/${id}`).then((response) => {
+          if (response.data.deletedCount) {
+            show === "adminList" && refetchAll();
+            show === "myList" && refetchAdmin();
+            Swal.fire("Deleted!", "Product has been deleted.", "success");
+          }
+        });
+      }
+    });
+  };
 
   return (
     <>
@@ -30,7 +61,10 @@ const TableBody = ({ data, index, show }) => {
               className="size-16 rounded"
             />
           </td>
-          <td className="text-center">{name}</td>
+          <td className="text-center">
+            {name}{" "}
+            <span className="text-green-600">{edited && "(Updated)"}</span>
+          </td>
           <td className="text-center">{category}</td>
           <td className="text-center">{formatNumber(price)}</td>
           <td className="text-center">{formatNumber(quantity)}</td>
@@ -42,7 +76,10 @@ const TableBody = ({ data, index, show }) => {
               <Link to={`/dashboard/products/update/${_id}`}>
                 <FaEdit className="text-2xl text-green-700 hover:text-green-500" />
               </Link>
-              <button className="p-0 border-0 ">
+              <button
+                className="p-0 border-0"
+                onClick={() => handleDelete(_id)}
+              >
                 <AiFillDelete className="text-2xl text-red-700 hover:text-red-500" />
               </button>
             </div>
