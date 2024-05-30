ea43581dad6745997530e0a57429de0f431552e0
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-30 22:59:42 +0600

    user pending items data show successfully

---
+++ src/hooks/useGetCartDataByUser.jsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+
+const useGetCartDataByUser = () => {
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+  const URL = `cart/${user?.email}`;
+  const {
+    data: userPendingItems = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["cart-user-products"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(URL);
+      return res.data;
+    },
+  });
+  return [userPendingItems, isLoading, refetch];
+};
+
+export default useGetCartDataByUser;
--- src/pages/dashboard/user/PendingItems.jsx
+++ src/pages/dashboard/user/PendingItems.jsx
@@ -1,9 +1,44 @@
 import BreadCum from '../../../components/dashboard/BreadCum';
+import TableWithHeader from '../../../components/dashboard/TableWithHeader';
+import useGetCartDataByUser from '../../../hooks/useGetCartDataByUser';
 
 const PendingItems = () => {
+   const tHeadData = [
+     {
+       id: 1,
+       field: "SL",
+     },
+     {
+       id: 2,
+       field: "Image",
+     },
+     {
+       id: 3,
+       field: "Name",
+     },
+     {
+       id: 4,
+       field: "Category",
+     },
+     {
+       id: 5,
+       field: "Price",
+     },
+     {
+       id: 6,
+       field: "Quantity",
+     },
+   ];
+  const [userPendingItems] = useGetCartDataByUser();
+
     return (
       <div>
         <BreadCum text1={"User Dashboard"} text2={"Pending Items"} />
+        <TableWithHeader
+          data={userPendingItems}
+          tHeadData={tHeadData}
+          show={"pendingOrders"}
+        />
       </div>
     );
 };
