dc24e3366f3adfe7de7cad13abe833d07f96410f
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-31 18:54:50 +0600

    cod page updated

--- src/components/dashboard/SSLCommercePayment.jsx
+++ src/components/dashboard/SSLCommercePayment.jsx
@@ -4,6 +4,9 @@ const SSLCommercePayment = () => {
     return (
       <div>
         <BreadCum text1={"User Dashboard"} text2={"SSL Payment"} />
+        <div className="bg-white flex items-center justify-center p-5 mt-5 rounded-lg h-[calc(100vh-177px)] overflow-auto ">
+            <h1 className="text-3xl font-semibold">Up Coming Feature</h1>
+        </div>
       </div>
     );
 };
--- src/components/dashboard/StripePayment.jsx
+++ src/components/dashboard/StripePayment.jsx
@@ -4,6 +4,9 @@ const StripePayment = () => {
     return (
       <div>
         <BreadCum text1={"User Dashboard"} text2={"Stripe Payment"} />
+        <div className="bg-white p-5 mt-5 rounded-lg h-[calc(100vh-177px)] overflow-auto ">
+            
+        </div>
       </div>
     );
 };
