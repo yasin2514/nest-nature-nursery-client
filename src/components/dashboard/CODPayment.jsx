9b2c2c118dccb3d7fce71f8a08ae6c1d330c3294
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-06-01 10:16:16 +0600

    double payment done

--- src/components/dashboard/CODPayment.jsx
+++ src/components/dashboard/CODPayment.jsx
@@ -1,14 +1,16 @@
 import BreadCum from "./BreadCum";
+import useGetCartDataByUser from "../../hooks/useGetCartDataByUser";
 
 const CODPayment = () => {
-    return (
-      <div>
-        <BreadCum text1={"User Dashboard"} text2={"COD Payment"} />
-        <div className="bg-white p-5 mt-5 rounded-lg h-[calc(100vh-177px)] overflow-auto ">
+    const [items] = useGetCartDataByUser();
 
-        </div>
-      </div>
-    );
+  console.log({ items });
+  return (
+    <div>
+      <BreadCum text1={"User Dashboard"} text2={"COD Payment"} />
+      <div className="bg-white p-5 mt-5 rounded-lg h-[calc(100vh-177px)] overflow-auto "></div>
+    </div>
+  );
 };
 
 export default CODPayment;
--- src/components/dashboard/StripePayment.jsx
+++ src/components/dashboard/StripePayment.jsx
@@ -1,14 +1,16 @@
+import useGetCartDataByUser from "../../hooks/useGetCartDataByUser";
 import BreadCum from "./BreadCum";
 
 const StripePayment = () => {
-    return (
-      <div>
-        <BreadCum text1={"User Dashboard"} text2={"Stripe Payment"} />
-        <div className="bg-white p-5 mt-5 rounded-lg h-[calc(100vh-177px)] overflow-auto ">
-            
-        </div>
-      </div>
-    );
+  const [items] = useGetCartDataByUser();
+  console.log({ items });
+
+  return (
+    <div>
+      <BreadCum text1={"User Dashboard"} text2={"Stripe Payment"} />
+      <div className="bg-white p-5 mt-5 rounded-lg h-[calc(100vh-177px)] overflow-auto "></div>
+    </div>
+  );
 };
 
 export default StripePayment;
