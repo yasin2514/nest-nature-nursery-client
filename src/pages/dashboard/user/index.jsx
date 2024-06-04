530c41692b0a2c50389c747f1669d7128c0e8dee
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-06-04 10:14:48 +0600

    user dashboard data show dynamically

--- src/pages/dashboard/user/index.jsx
+++ src/pages/dashboard/user/index.jsx
@@ -3,14 +3,29 @@ import Box from "../../../components/dashboard/Box";
 import BreadCum from "../../../components/dashboard/BreadCum";
 import PendingAndPurchaseChart from "../../../components/dashboard/PendingAndPurchaseChart";
 import useGetCartDataByUser from "../../../hooks/useGetCartDataByUser";
+import useGetPaymentInfoByUser from "../../../hooks/useGetPaymentInfoByUser";
 import useNumberFormatter from "../../../hooks/useNumberFormatter";
 
 const UserDashboard = () => {
-  const [userPendingItems] = useGetCartDataByUser();
   const formatNumber = useNumberFormatter();
+  const [userPendingItems] = useGetCartDataByUser();
+  const [paymentUserData] = useGetPaymentInfoByUser();
 
-  const totalPendingAmount = userPendingItems?.reduce(
+  const totalCartAmount = userPendingItems?.reduce(
     (acc, item) => acc + item?.totalAmount,
+    0
+  );
+
+  const totalPurchasedAmount = paymentUserData?.reduce(
+    (acc, item) => acc + item?.totalAmount,
+    0
+  );
+  const totalPaidAmount = paymentUserData?.reduce(
+    (acc, item) => acc + item?.totalAmount,
+    0
+  );
+  const totalDueAmount = paymentUserData?.reduce(
+    (acc, item) => acc + item?.totalDue,
     0
   );
 
@@ -20,19 +35,23 @@ const UserDashboard = () => {
       <div className="flex flex-col justify-around h-[calc(100vh-165px)]">
         <div className="mt-6 grid grid-cols-12 gap-8">
           <Box
-            value={userPendingItems?.length || 0}
-            title={"Pending Items"}
+            value={`$${formatNumber(totalCartAmount || 0)}`}
+            title={"Cart Pending Amount"}
             className={"bg-[#2c8d2c]"}
           />
           <Box
-            value={`$${formatNumber(totalPendingAmount)}`}
-            title={"Total Pending Amount"}
+            value={`$${formatNumber(totalPurchasedAmount || 0)}`}
+            title={"Purchased Amount"}
             className={"bg-[#786fc4]"}
           />
-          <Box value={10} title={"Purchase Items"} className={"bg-[#ce4b3a]"} />
           <Box
-            value={10}
-            title={"Total Purchased Amount"}
+            value={`$${formatNumber(totalPaidAmount || 0)}`}
+            title={"Paid Amount"}
+            className={"bg-[#ce4b3a]"}
+          />
+          <Box
+            value={`$${formatNumber(totalDueAmount || 0)}`}
+            title={"Due Amount"}
             className={"bg-[#16a7c0]"}
           />
         </div>
