5002b7e0ca9faeed8d93606d677191959283e6aa
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-06-04 21:59:50 +0600

    admin dashboard  delivery section done

--- src/pages/dashboard/admin/PendingDelivery.jsx
+++ src/pages/dashboard/admin/PendingDelivery.jsx
@@ -3,51 +3,51 @@ import TableWithHeader from "../../../components/dashboard/TableWithHeader";
 import useGetPaymentInfo from "../../../hooks/useGetPaymentInfo";
 
 const PendingDelivery = () => {
-           const tHeadData = [
-             {
-               id: 1,
-               field: "SL",
-             },
-             {
-               id: 2,
-               field: "Payment ID",
-             },
-             {
-               id: 3,
-               field: "Amount",
-             },
-             {
-               id: 4,
-               field: "Payment Status",
-             },
-             {
-               id: 5,
-               field: "Payment Date",
-             },
-             {
-               id: 6,
-               field: "Delivery Status",
-             },
-             {
-               id: 7,
-               field: "Action",
-             },
-           ];
-           const [paymentData] = useGetPaymentInfo();
-            const PendingDeliveryData = paymentData?.filter(
-              (item) => item?.delivery === "Pending"
-            );
+  const tHeadData = [
+    {
+      id: 1,
+      field: "SL",
+    },
+    {
+      id: 2,
+      field: "Payment ID",
+    },
+    {
+      id: 3,
+      field: "Amount",
+    },
+    {
+      id: 4,
+      field: "Payment Status",
+    },
+    {
+      id: 5,
+      field: "Payment Date",
+    },
+    {
+      id: 6,
+      field: "Delivery Status",
+    },
+    {
+      id: 7,
+      field: "Action",
+    },
+  ];
+  const [paymentData] = useGetPaymentInfo();
+  const PendingDeliveryData = paymentData?.filter(
+    (item) => item?.delivery === "Pending"
+  );
 
-           return (
-             <div>
-               <BreadCum text1={"User Dashboard"} text2={"Pending Delivery"} />
-               <TableWithHeader
-                 data={PendingDeliveryData || []}
-                 tHeadData={tHeadData}
-                 show={"paymentInfoAdmin"}
-               />
-             </div>
-           );
+  return (
+    <div>
+      <BreadCum text1={"User Dashboard"} text2={"Pending Delivery"} />
+      <TableWithHeader
+        data={PendingDeliveryData || []}
+        tHeadData={tHeadData}
+        show={"paymentInfoAdmin"}
+      />
+    </div>
+  );
 };
 
 export default PendingDelivery;
