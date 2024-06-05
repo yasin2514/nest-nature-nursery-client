fc3ac0f38e238dbce52bf541ebab4c1c9b71c83f
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-06-05 18:37:09 +0600

    admin dashboard done

--- src/pages/dashboard/admin/index.jsx
+++ src/pages/dashboard/admin/index.jsx
@@ -2,32 +2,80 @@ import BarChartComponent from "../../../components/dashboard/BarChartComponent";
 import Box from "../../../components/dashboard/Box";
 import BreadCum from "../../../components/dashboard/BreadCum";
 import PieChart from "../../../components/dashboard/PieChart";
+import useGetCartProducts from "../../../hooks/useGetCartProducts";
 import useGetDataUploadedByAdmin from "../../../hooks/useGetDataUploadedByAdmin";
+import useGetPaymentInfo from "../../../hooks/useGetPaymentInfo";
 import useGetProducts from "../../../hooks/useGetProducts";
+import useGetPurchaseItem from "../../../hooks/useGetPurchaseItem";
+import useNumberFormatter from "../../../hooks/useNumberFormatter";
 
 const AdminDashboard = () => {
   const [products] = useGetProducts();
   const [myProducts] = useGetDataUploadedByAdmin();
+  const [paymentData] = useGetPaymentInfo();
+  const [pendingCart] = useGetCartProducts();
+  const [purchaseItems] = useGetPurchaseItem();
+  const formatNumber = useNumberFormatter();
+
+  const totalSellAmount = paymentData.reduce((acc, item) => {
+    return acc + item?.totalAmount;
+  }, 0);
+  const totalDueAmount = paymentData.reduce((acc, item) => {
+    return acc + item?.totalDue;
+  }, 0);
+
+  const pendingDelivery = paymentData?.filter(
+    (item) => item?.delivery === "Pending"
+  );
+  const completedDelivery = paymentData?.filter(
+    (item) => item?.delivery === "Success"
+  );
   return (
     <div className=" h-full">
       <BreadCum text1={"Admin Dashboard"} text2={"Home"} />
       <div className="flex flex-col justify-around h-[calc(100vh-165px)]">
         <div className="mt-6 grid grid-cols-12 gap-8">
           <Box
-            value={products?.length || 0}
-            title={"Total Products"}
+            value={myProducts?.length || 0}
+            title={"My Products"}
             className={"bg-[#2c8d2c]"}
           />
           <Box
-            value={myProducts?.length || 0}
-            title={"My Products"}
+            value={products?.length || 0}
+            title={"Total Products"}
             className={"bg-[#16a7c0]"}
           />
-          <Box value={10} title={"Completed Orders"} className={"bg-[#786fc4]"} />
           <Box
-            value={10}
+            value={`$${formatNumber(totalSellAmount || 0)}`}
             title={"Total Sell Amount"}
+            className={"bg-[#786fc4]"}
+          />
+          <Box
+            value={`$${formatNumber(totalDueAmount || 0)}`}
+            title={"Total Due Amount"}
             className={"bg-[#ce4b3a]"}
+          />
+        </div>
+        <div className="mt-6 grid grid-cols-12 gap-8">
+          <Box
+            value={pendingCart?.length || 0}
+            title={"Unprocessed Orders"}
+            className={"bg-[#786fc4]"}
+          />
+          <Box
+            value={purchaseItems?.length || 0}
+            title={"Completed Orders"}
+            className={"bg-[#ce4b3a]"}
+          />
+          <Box
+            value={pendingDelivery?.length || 0}
+            title={"Pending Delivery"}
+            className={"bg-[#2c8d2c]"}
+          />
+          <Box
+            value={completedDelivery?.length || 0}
+            title={"Completed Delivery"}
+            className={"bg-[#16a7c0]"}
           />
         </div>
 
