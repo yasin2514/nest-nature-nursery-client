0cff968209f6a6349cd860113011a4dda53c676e
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-06-06 17:44:59 +0600

    updated private route

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,13 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import Loading from "../components/ui/Loading";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const location = useLocation();
-
+  if (loading) {
+    return <Loading />;
+  }
   if (user) {
     return children;
   }
