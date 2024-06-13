31002a57954b2a2a8a9323596f222b70cd56be84
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-06-13 19:45:19 +0600

    updateed loading

--- src/components/ui/Loading.jsx
+++ src/components/ui/Loading.jsx
@@ -1,7 +1,7 @@
 
 const Loading = () => {
     return (
-      <div className="flex items-center justify-center h-screen bg-gray-400">
+      <div className="flex items-center justify-center h-screen">
         <span className="loading loading-ring loading-xs"></span>
         <span className="loading loading-ring loading-sm"></span>
         <span className="loading loading-ring loading-md"></span>
